141eb477b2c51dfc562a0807f0387977935c0129
michael barrett <michaelbarrett343@googlemail.com>
2021-07-31 12:29:34 +0100

    created garden model and controller with routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :gardens, except: [:destroy]
+  resources :users do
+    resources :gardens, except: [:destroy]
+  end
+  resources :gardens, only: [:destroy]
 end
