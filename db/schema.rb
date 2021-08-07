6bba03a6e3ae7d8c4e95c2e92d7a2c2cdb8ff3a1
michael barrett <michaelbarrett343@googlemail.com>
2021-08-07 11:45:04 +0100

    added description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_04_220201) do
+ActiveRecord::Schema.define(version: 2021_08_07_103831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_220201) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.text "description"
     t.index ["user_id"], name: "index_gardens_on_user_id"
   end
 
